0455a388b83edb9b0497fce4cf6b71d460ffc2be
youssefazzouz <youssef.azzouz@infor.com>
2021-03-16 16:48:28 +0100

    remove builds

--- build/lib/src/db/exceptions.py
+++
@@ -1,7 +0,0 @@
-import logging as lg
-
-_logger = lg.getLogger(__name__)
-
-class DbConnectionException(Exception): # this is for redis 
-    # _logger.error("error occured while connecting to database")
-    pass
--- build/lib/src/scrap/statusEnum.py
+++
@@ -1,6 +0,0 @@
-import enum
-
-class StateWorker(enum.Enum):
-    running = 0 
-    pending = 1 # worker in a failed state pending instructions
-    aborted = 2 # failure without resolution
